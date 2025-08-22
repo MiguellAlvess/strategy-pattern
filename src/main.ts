d0a23ce11bdacb5e3e2fab5174b91aa485b725e5
Miguel Alves <miguellalvesiqueira@gmail.com>
2025-08-22 16:32:36 -0300

    feat: implement tax calculation endpoints

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,23 @@
 import express from "express"
+import { TaxCalculator } from "@/service/tax-calculator"
+import { TaxController } from "@/controller/tax-controller"
 
 const app = express()
 
-app.get("/", (req, res) => {
-  res.send("Hello World")
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+const taxCalculator = new TaxCalculator()
+const taxController = new TaxController(taxCalculator)
+
+app.post("/tax/calculate", (req, res) => {
+  taxController.calculateTax(req, res)
 })
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000")
+app.get("/tax/types", (req, res) => {
+  taxController.getAvailableTaxTypes(req, res)
+})
+
+app.listen(() => {
+  console.log(`Server is running on port 3000`)
 })
